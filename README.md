aa927b24e0ec4915c247745e481f19cb74fa2ec7
jaehyeongAN <nonamed000000@gmail.com>
2022-03-11 19:38:38 +0900

    update script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DocumentExtracter
+
+### Run server 
+```
+# run server in local server
+sh run_server.sh local
+
+# run server in local and background running
+sh run_server.sh local background
+```
+- args1 : ["local", "external", "https"]
+- args2 : ["background"]
--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,32 @@
-nohup uvicorn main:app --host 0.0.0.0 --port 8001 --ssl-keyfile ./key.pem --ssl-certfile ./cert.pem &
+#!/bin/bash
+
+
+args=("$@")
+
+if [ "external" == ${args[0]} ];then
+	if [ ${args[1]} ] && [ "background" == ${args[1]} ];then
+		echo "Run server with HTTP and Background runnning"
+		nohup uvicorn main:app --host 0.0.0.0 --port 8001 &
+	else
+		echo "Run server with HTTP"
+		uvicorn main:app --host 0.0.0.0 --port 8001
+	fi
+elif [ "https" == ${args[0]} ];then
+	if [ ${args[1]} ] && [ "background" == ${args[1]} ];then
+		echo "Run server with HTTPS and Background runnning"
+		nohup uvicorn main:app --host 0.0.0.0 --port 8001 --ssl-keyfile ./key.pem --ssl-certfile ./cert.pem &
+	else
+		echo "Run server with HTTPS"
+		uvicorn main:app --host 0.0.0.0 --port 8001 --ssl-keyfile ./key.pem --ssl-certfile ./cert.pem
+	fi
+elif [ "local" == ${args[0]} ];then
+	if [ ${args[1]} ] && [ "background" == ${args[1]} ];then
+		echo "Run server in Local and Background runnning"
+		nohup uvicorn main:app --host 0.0.0.0 --port 8001 &
+	else
+		echo "Run server in Local"
+		uvicorn main:app --host 0.0.0.0 --port 8001
+	fi
+else
+	echo "invaild parameter."
+fi
